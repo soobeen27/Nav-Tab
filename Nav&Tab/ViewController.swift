792ca025333d791709713b3c9de584be889c005f
soobeen27 <youdew27@icloud.com>
2022-10-17 17:21:30 +0900

    Initial Commit

---
+++ Nav&Tab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nav&Tab
+//
+//  Created by Soo Jang on 2022/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
